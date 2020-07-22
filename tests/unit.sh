aff20d282fa60efc3136fed161b0c29e29dd6c5b
Gaby Molina Goigoux <gabymg@gabymg.es>
2020-07-22 23:40:25 +0000

    Write testCreatesDatabase function

--- tests/unit.sh
+++ tests/unit.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Run only the functions that we test
-source ../functions.sh
+source "../functions.sh"
 
 # Test we can set environment variables from key/value pairs exported from a
 # text file
@@ -13,11 +13,19 @@ testSetsEnvVarsFromTextFile () {
 	DB=foo
 	EOM
 
-	set_envars /tmp/envars.txt
+	gaea-set-envars /tmp/envars.txt
 
 	assertSame 'example.com' "$DOMAIN"
 	assertSame 'foo' "$DB"
-}	
+}
+
+testCreatesDatabase () {
+	DB_NAME=bar
+	gaea-db-create $DB_NAME
+
+	IS_DB_CREATED=$( mysql -u root --skip-column-names -e "SELECT CASE WHEN EXISTS ( SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$DB_NAME' ) THEN '0' ELSE '1' END" )
+	assertTrue "$IS_DB_CREATED"
+}
 
 # Load shUnit2
 . "/usr/local/src/shunit2/shunit2"
