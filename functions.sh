be8535cf3897404f1a5729f89359ebd67d0b62f4
Gaby Molina Goigoux <gabymg@gabymg.es>
2020-07-23 17:15:27 +0000

    Create the database user

--- functions.sh
+++ functions.sh
@@ -1,5 +1,16 @@
 # Sets Environment Variables from a Text File
-set_envars () {
+gaea-set-envars () {
 	source $1
 	export $(cut -d= -f1 $1)
+}
+
+
+# Creates a database and a user with all permissions
+#
+# gaea-db-create dbname dbuser dbuserpass
+gaea-db-create () {
+	
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $1 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+
+	mysql -u root -e "GRANT ALL ON $1.* TO '$2'@'localhost' IDENTIFIED BY '$3'"
 }
