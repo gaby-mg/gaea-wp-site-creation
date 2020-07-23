8460c3cf72ee3d32939cc7c3aa5b056a71db3f0f
Gaby Molina Goigoux <gabymg@gabymg.es>
2020-07-23 18:38:44 +0000

    Test if the database user has required permissions

--- tests/unit.sh
+++ tests/unit.sh
@@ -23,16 +23,23 @@ testCreatesDatabaseAndUser () {
 	local DB_NAME=foo
 	local DB_USER=bar
 	local DB_USERPASS='12345678a-zA-Z'
+	local DB_PERMS="GRANT ALL PRIVILEGES"
 
 	gaea-db-create $DB_NAME $DB_USER $DB_USERPASS
 
 	IS_DB_CREATED=$( mysql -u root --skip-column-names -e "SELECT CASE WHEN EXISTS ( SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$DB_NAME' ) THEN '0' ELSE '1' END" )
 
 	IS_DBUSER_CREATED=$( mysql -u root --skip-column-names -e "SELECT CASE WHEN EXISTS ( SELECT * FROM mysql.user WHERE User = '$DB_USER' ) THEN '0' ELSE '1' END" )
+
+	DB_USER_PERMS=$( mysql -u root --skip-column-names -e "SHOW GRANTS FOR '$DB_USER'@'localhost'" )
 	
 	
 	assertTrue "The database wasn't created" "$IS_DB_CREATED"
 	assertTrue "The database user wasn't created" "$IS_DBUSER_CREATED"
+	assertContains "The database user hasn't required permissions" "$DB_USER_PERMS" "$DB_PERMS"
+
+	mysql -u root -e "DROP DATABASE $DB_NAME"
+	mysql -u root -e "DROP USER $DB_USER@localhost"
 }
 
 # Load shUnit2
