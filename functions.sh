b37ac94fc5fa9d8dc9df5c3e896f03ad4c95af73
Gaby Molina Goigoux <gabymg@gabymg.es>
2020-07-25 12:47:10 +0000

    Finds unique credentials for a new database

--- functions.sh
+++ functions.sh
@@ -4,6 +4,45 @@ gaea-set-envars () {
 	export $(cut -d= -f1 $1)
 }
 
+# Creates a random alphanumric string of the given length
+gaea-random-string () {
+	CHARSET="a-zA-Z0-9"
+	LENGTH=$1
+
+	< /dev/urandom tr -dc $CHARSET | head -c $LENGTH
+}
+
+# Checks if a database with a given name already exists
+gaea-db-exists () {
+
+	local IS_DB_CREATED=$( mysql -u root --skip-column-names -e "SELECT CASE WHEN EXISTS ( SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$1' ) THEN '0' ELSE '1' END" )
+
+	if [ "$IS_DB_CREATED" = '0' ]; then
+		return 0
+	fi
+
+	return 1
+}
+
+# Finds a unique database and password
+gaea-db-new () {
+	local DB_CREDENTIALS_FILE='/tmp/db-credentials.txt'
+	local DB_NAME="$( gaea-random-string 20 )"
+	local DB_USER="$( gaea-random-string 10 )"
+
+	while [ $( gaea-db-exists $DB_NAME ) ]
+	do
+		DB_NAME="$( gaea-random-string 20 )"
+	done
+
+	touch "$DB_CREDENTIALS_FILE"
+	
+	echo "DB_NAME=$DB_NAME" > "$DB_CREDENTIALS_FILE"
+	echo 'DB_USER='$DB_NAME'_'$DB_USER >> "$DB_CREDENTIALS_FILE"
+}
+
+# 
+
 
 # Creates a database and a user with all permissions
 #
