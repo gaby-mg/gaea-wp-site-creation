5ef9524901d9ba51c903c35e52ad8b8ad3112ef9
Gaby Molina Goigoux <gabymg@gabymg.es>
2021-03-04 21:00:41 +0000

    Initial import of pre-existing web files

--- functions.sh
+++ functions.sh
@@ -12,6 +12,14 @@ gaea-random-string () {
 	< /dev/urandom tr -dc $CHARSET | head -c $LENGTH
 }
 
+
+gaea-random-password () {
+	CHARSET="a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?="
+	LENGTH=$1
+
+	< /dev/urandom tr -dc $CHARSET | head -c $LENGTH
+}
+
 # Checks if a database with a given name already exists
 gaea-db-exists () {
 
@@ -26,22 +34,19 @@ gaea-db-exists () {
 
 # Finds a unique database and password
 gaea-db-new () {
-	local DB_CREDENTIALS_FILE='/tmp/db-credentials.txt'
 	local DB_NAME="$( gaea-random-string 20 )"
 	local DB_USER="$( gaea-random-string 10 )"
+	local DB_USERPASS="$( gaea-random-password 18 )"
 
 	while [ $( gaea-db-exists $DB_NAME ) ]
 	do
 		DB_NAME="$( gaea-random-string 20 )"
 	done
 
-	touch "$DB_CREDENTIALS_FILE"
-	
-	echo "DB_NAME=$DB_NAME" > "$DB_CREDENTIALS_FILE"
-	echo 'DB_USER='$DB_NAME'_'$DB_USER >> "$DB_CREDENTIALS_FILE"
+	echo 'GAEA_DB_NAME="'$DB_NAME'"' >> "$GAEA_ENVARS_FILE"
+	echo 'GAEA_DB_USER="'$DB_NAME'_'$DB_USER'"' >> "$GAEA_ENVARS_FILE"
+	echo 'GAEA_DB_USERPASS="'$DB_USERPASS'"' >> "$GAEA_ENVARS_FILE"
 }
-
-# 
 
 
 # Creates a database and a user with all permissions
@@ -49,7 +54,80 @@ gaea-db-new () {
 # gaea-db-create dbname dbuser dbuserpass
 gaea-db-create () {
 	
-	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $1 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $GAEA_DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
 
-	mysql -u root -e "GRANT ALL ON $1.* TO '$2'@'localhost' IDENTIFIED BY '$3'"
+	mysql -u root -e "GRANT ALL ON $GAEA_DB_NAME.* TO '$GAEA_DB_USER'@'localhost' IDENTIFIED BY '$GAEA_DB_USERPASS'"
+}
+
+gaea-apache-wp-cfg-file () {
+	local TMP_FILE='/tmp/gaea-apache-config.txt'
+	cat > $TMP_FILE <<-EOF
+	<VirtualHost *:80>
+        	ServerName test.$GAEA_DOMAIN
+
+        	ServerAdmin hosting@gabymg.es
+        	DocumentRoot /srv/www/$GAEA_DOMAIN
+        	ErrorLog \${APACHE_LOG_DIR}/error.log
+        	CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        	# Enable .htacces Overrides
+        	<Directory /srv/www/$GAEA_DOMAIN>
+                	AllowOverride All
+        	</Directory>
+	</VirtualHost>
+	EOF
+}
+
+# Generates an Apache config file for a given domain
+gaea-apache-cfg-file () {
+	local APACHE_CONFIG_FILE="/etc/apache2/sites-available/$GAEA_DOMAIN.conf"
+	local TMP_FILE='/tmp/gaea-apache-config.txt'
+
+	case "$1" in
+		'wordpress')
+			gaea-apache-wp-cfg-file
+			;;
+	esac
+
+	cp "$TMP_FILE" "$APACHE_CONFIG_FILE"
+	rm $TMP_FILE
+}
+
+gaea-docroot () {
+	local DIRECTORY="/srv/www/$GAEA_DOMAIN"
+	mkdir "$DIRECTORY"
+
+	echo "Hello, World!" > "$DIRECTORY/index.html"
+
+	chown -R gabymg:gabymg $DIRECTORY
+}
+
+gaea-wp-new () {
+	local WP_ADMIN="$( gaea-random-string 8 )"
+	local WP_ADMIN_PASSWORD="$( gaea-random-password 18 )"
+	local WP_ADMIN_EMAIL='wordpress@gabymg.es'
+
+	echo 'GAEA_WP_ADMIN="'$WP_ADMIN'"' >> "$GAEA_ENVARS_FILE"
+	echo 'GAEA_WP_ADMIN_PASSWORD="'$WP_ADMIN_PASSWORD'"' >> "$GAEA_ENVARS_FILE"
+	echo 'GAEA_WP_ADMIN_EMAIL="'$WP_ADMIN_EMAIL'"' >> "$GAEA_ENVARS_FILE"
+}
+
+
+gaea-wp-install () {
+
+	local GAEA_LOCALE='es_ES'
+
+	rm /srv/www/$GAEA_DOMAIN/index.html
+
+	# Change ownership in order to use WP-CLI
+	sudo chown -R gabymg:gabymg /srv/www/$GAEA_DOMAIN
+	
+	# Download WordPress
+	sudo -u gabymg -i -- wp core download --locale=es_ES --path=/srv/www/$GAEA_DOMAIN
+
+	# Create wp-config.php
+	sudo -u gabymg -i -- wp config create --dbname=$GAEA_DB_NAME --dbuser=$GAEA_DB_USER --dbpass=$GAEA_DB_USERPASS --locale=$GAEA_LOCALE --path=/srv/www/$GAEA_DOMAIN
+
+	# Install the site
+	sudo -u gabymg -i -- wp core install --url=test.$GAEA_DOMAIN --title=$GAEA_TITLE --admin_user=$GAEA_WP_ADMIN --admin_email=$GAEA_WP_ADMIN_EMAIL --admin_password=$GAEA_WP_ADMIN_PASSWORD --path=/srv/www/$GAEA_DOMAIN
 }
---
+++ siteCreation.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+GAEA_ENVARS_FILE='/tmp/envars.txt'
+
+# Source functions file
+source "functions.sh"
+
+# Get environmental variables
+echo "Getting environmental variables..."
+gaea-db-new
+gaea-wp-new
+
+# Set environmental variables
+echo "Setting environmental variables..."
+gaea-set-envars "/tmp/envars.txt"
+
+# Set up Apache configuration file
+echo "Setting up the Apache configuration file..."
+gaea-apache-cfg-file 'wordpress'
+
+# Set up document root
+echo "Setting up website document root..."
+gaea-docroot
+
+# Start the web site
+echo "Enabling the website..."
+a2ensite --quiet $GAEA_DOMAIN.conf
+systemctl reload apache2
+
+# Create the database
+echo "Creating the database..."
+gaea-db-create
+
+# Install WordPress
+echo "Installing WordPress..."
+gaea-wp-install
