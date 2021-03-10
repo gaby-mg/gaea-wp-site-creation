0e4007b6d0805b264f9ae8a4b85e0f612f3f01de
Gaby Molina Goigoux <gabymg@gabymg.es>
2021-03-10 18:10:06 +0100

    Refactor for parameter use

--- gaea-site.sh
+++ gaea-site.sh
@@ -5,32 +5,47 @@ GAEA_ENVARS_FILE='/tmp/envars.txt'
 # Source functions file
 source "functions.sh"
 
-# Get environmental variables
-echo "Getting environmental variables..."
-gaea-db-new
-gaea-wp-new
+if [ -n "$1" ]; then
+	while [ -n "$1" ]; do
+		case "$1" in
+			create)
+				# Get environmental variables
+				echo "Getting environmental variables..."
+				gaea-db-new
+				gaea-wp-new
 
-# Set environmental variables
-echo "Setting environmental variables..."
-gaea-set-envars "/tmp/envars.txt"
+				# Set environmental variables
+				echo "Setting environmental variables..."
+				gaea-set-envars "/tmp/envars.txt"
 
-# Set up Apache configuration file
-echo "Setting up the Apache configuration file..."
-gaea-apache-cfg-file 'wordpress'
+				# Set up Apache configuration file
+				echo "Setting up the Apache configuration file..."
+				gaea-apache-cfg-file 'wordpress'
 
-# Set up document root
-echo "Setting up website document root..."
-gaea-docroot
+				# Set up document root
+				echo "Setting up website document root..."
+				gaea-docroot
 
-# Start the web site
-echo "Enabling the website..."
-a2ensite --quiet $GAEA_DOMAIN.conf
-systemctl reload apache2
+				# Start the web site
+				echo "Enabling the website..."
+				a2ensite --quiet $GAEA_DOMAIN.conf
+				systemctl reload apache2
 
-# Create the database
-echo "Creating the database..."
-gaea-db-create
+				# Create the database
+				echo "Creating the database..."
+				gaea-db-create
 
-# Install WordPress
-echo "Installing WordPress..."
-gaea-wp-install
+				# Install WordPress
+				echo "Installing WordPress..."
+				gaea-wp-install
+				;;
+			*) echo "Option not recognized"
+		esac
+
+		shift
+	done
+else
+	echo "No parameters found"
+fi
+
+
